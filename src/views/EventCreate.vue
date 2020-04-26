55faad43eedadec279cbfa47ffa6e67adb2b47eb
Beata Karolczak <karolczak.beata@gmail.com>
2020-04-26 15:41:19 +0200

    Add form in EventCreate

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,28 +1,103 @@
 <template>
   <div>
-    <h1>Create Event, {{ user.name }}</h1>
-    <p>This event was created by {{ user.id }}</p>
-    <p>There are {{ catLength }} categories</p>
-    <p>{{ getEventById(2) }}</p>
-    <ul>
-      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
-    </ul>
+    <h1>Create Event</h1>
+    <form>
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
+      </div>
+      <div class="field">
+        <label>Description</label>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date" />
+      </div>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex' // to map state into computed properties
+import Datepicker from 'vuejs-datepicker'
+// import { mapState, mapGetters } from 'vuex' // to map state into computed properties
 
 export default {
-  computed: {
-    catLength() {
-      // return this.$store.categories.length
-      return this.$store.getters.catLength
-    },
-    ...mapGetters(['getEventById']),
-    ...mapState(['user', 'categories'])
+  components: {
+    Datepicker
+  },
+  data() {
+    const times = []
+    for (let i = 1; i <= 24; i++) {
+      times.push(i + ':00')
+    }
+    return {
+      times,
+      categories: this.$store.state.categories,
+      event: this.createFreshEventObject()
+    }
+  },
+  methods: {
+    createFreshEventObject() {
+      const user = this.$store.state.user
+      const id = Math.floor(Math.random() * 10000000)
+      return {
+        id: id,
+        user: user,
+        category: '',
+        organizer: user,
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
+    }
   }
+  // computed: {
+  //   catLength() {
+  //     // return this.$store.categories.length
+  //     return this.$store.getters.catLength
+  //   },
+  //   ...mapGetters(['getEventById']),
+  //   ...mapState(['user', 'categories'])
+  // }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.field {
+  margin-bottom: 24px;
+}
+</style>
