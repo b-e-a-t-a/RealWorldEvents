c6ca038b5e7ea85d4e62b409b9e82e8eab64b493
Beata Karolczak <karolczak.beata@gmail.com>
2020-06-23 13:49:29 +0200

    Add mapActions helper

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -29,15 +29,16 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 export default {
   props: ['id'],
   created() {
-    this.$store.dispatch('event/fetchEvent', this.id)
+    this.fetchEvent(this.id)
   },
   computed: mapState({
     event: state => state.event.event
-  })
+  }),
+  methods: mapActions('event', ['fetchEvent'])
 }
 </script>
 
